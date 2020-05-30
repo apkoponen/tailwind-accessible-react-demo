bdfa548dec362057535fb2b239f6ef2fedec0fe2
Ari-Pekka Koponen <ap.koponen@gmail.com>
2020-05-30 23:48:29 +0300

    Fix any errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,13 @@
-export function isFunction(value): value is Function {
+export function isFunction(
+  //@ts-ignore
+  value: any
+): value is Function {
   return value && {}.toString.call(value) === "[object Function]";
 }
 
-export function isString(value): value is string {
+export function isString(
+  //@ts-ignore
+  value: any
+): value is string {
   return typeof value === "string" || value instanceof String;
 }
